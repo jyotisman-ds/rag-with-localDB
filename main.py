146f4446954a75f7d4a2840affb8dffdf949c9fe
jyotisman-ds <jsahoo@iastate.edu>
2024-12-13 18:30:51 -0500

    load and split document into chunks

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+load_dotenv()
+
+
 def main():
+    """main function to execute"""
     print("Hello from rag-with-localdb!")
+    # llm = ChatOpenAI()
+    path_to_pdf = "2210.03629v3.pdf"
+    pdf_loader = PyPDFLoader(file_path=path_to_pdf)
+    pdf_text = pdf_loader.load()  # Load the PDF file (does not do any chunking)
+
+    # Split the text into chunks
+    splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+    docs = splitter.split_documents(documents=pdf_text)
 
 
 if __name__ == "__main__":
