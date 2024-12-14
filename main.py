7c2f05f809c7cc4f2faba571f2fe232146f020dd
jyotisman-ds <jsahoo@iastate.edu>
2024-12-13 19:04:16 -0500

    faiss local vector db creation and rest of RAG setup

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import os
+
 from dotenv import load_dotenv
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAI, OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
 load_dotenv()
@@ -16,6 +23,32 @@ def main():
     # Split the text into chunks
     splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
     docs = splitter.split_documents(documents=pdf_text)
+
+    embeddings = OpenAIEmbeddings()
+    # vectorize and store the vectors in-memory
+    vectorstore = None
+    if not os.path.exists("faiss_index_react"):
+        vectorstore = FAISS.from_documents(docs, embeddings)
+    if vectorstore is not None:
+        # persist for later use
+        vectorstore.save_local("faiss_index_react")
+
+    new_vectorstore = FAISS.load_local(
+        "faiss_index_react", embeddings, allow_dangerous_deserialization=True
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(
+        OpenAI(), retrieval_qa_chat_prompt
+    )
+    retrieval_chain = create_retrieval_chain(
+        new_vectorstore.as_retriever(), combine_docs_chain
+    )
+
+    res = retrieval_chain.invoke(
+        {"input": "Describe the principles of ReAct in 5 sentences"}
+    )
+    print(res["answer"])
 
 
 if __name__ == "__main__":
